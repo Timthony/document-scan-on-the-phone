b919c51e47b0740a23bc7f14a32ab6627ea88f73
Timthony <tianhangz1023@gmail.com>
2018-11-13 22:43:19 +0800

    完成hed.py

--- hed_net.py
+++ hed_net.py
@@ -33,7 +33,7 @@ def class_balanced_sigmoid_cross_entropy(logits, label):
         beta = count_neg/(count_neg + count_pos)
         pos_weight = beta/(1.0-beta)
         cost = tf.nn.weighted_cross_entropy_with_logits(logits=logits, targets=label, pos_weight=pos_weight)
-
+        cost = tf.reduce_mean(cost * (1-beta))
         # 如果样本中1的数量等于0，那就直接让 cost 为 0，因为 beta == 1 时， 除法 pos_weight = beta / (1.0 - beta) 的结果是无穷大
         zero = tf.equal(count_pos, 0.0)
         final_cost = tf.where(zero, 0.0, cost)
@@ -50,6 +50,138 @@ def mobilenet_v2_style_hed(inputs, batch_size, is_training):
         weights_regularizer = None
 
     func_blocks = mobilenet_v2_func_blocks(is_training)
+
+    _conv2d = func_blocks['conv2d']
+    _inverted_residual_block = func_blocks['inverted_residual_block']
+    _avg_pool2d = func_blocks['avg_pool2d']
+    filter_initializer = func_blocks['filter_initializer']
+    activation_func = func_blocks['activation_func']
+    ####################################################
+
+    def _dsn_1x1_conv2d(inputs, filters):
+        kernel_size = [1, 1]
+        outputs = tf.layers.conv2d(inputs, filters, kernel_size,
+                                   padding='same',
+                                   activation=None,
+                                   use_bias=False,
+                                   kernel_initializer=filter_initializer,
+                                   kernel_regularizer=weights_regularizer)
+        outputs = tf.layers.batch_normalization(outputs, training=is_training)
+
+        return outputs
+
+    def _output_1x1_conv2d(inputs, filters):
+        kernel_size = [1, 1]
+        outputs = tf.layers.conv2d(inputs, filters, kernel_size,
+                                   padding='same',
+                                   activation=None,
+                                   use_bias=True,
+                                   kernel_initializer=filter_initializer,
+                                   kernel_regularizer=weights_regularizer)
+
+        return outputs
+
+    def _dsn_deconv2d_with_upsample_factor(inputs, filters, upsample_factor):
+        ## https://github.com/s9xie/hed/blob/master/examples/hed/train_val.prototxt
+        ## 从这个原版代码里看，是这样计算 kernel_size 的
+        kernel_size = [2 * upsample_factor, 2 * upsample_factor]
+        outputs = tf.layers.conv2d_transpose(inputs,
+                                             filters,
+                                             kernel_size,
+                                             strides=(upsample_factor, upsample_factor),
+                                             padding='same',
+                                             activation=None,  ## no activation
+                                             use_bias=True,  ## use bias
+                                             kernel_initializer=filter_initializer,
+                                             kernel_regularizer=weights_regularizer)
+
+        ## 概念上来说，deconv2d 已经是最后的输出 layer 了，只不过最后还有一步 1x1 的 conv2d 把 5 个 deconv2d 的输出再融合到一起
+        ## 所以不需要再使用 batch normalization 了
+
+        return outputs
+
+    with tf.variable_scope('hed', 'hed', [inputs]):
+        end_points = {}
+        net = inputs
+
+        # mobilenet v2 as basenet
+        with tf.variable_scope('mobilenet_v2'):
+        # 标准的 mobilenet v2 里面并没有这两层，
+        # 这里是为了得到和 input image 相同 size 的 feature map 而增加的层
+            net = _conv2d(net, 3, [3, 3], stride=1, scope='block0_0')
+            net = _conv2d(net, 6, [3, 3], stride=1, scope='block0_1')
+
+            dsn1 = net
+            net = _conv2d(net, 12, [3, 3], stride=2, scope='block0_2') # size/2
+
+            net = _inverted_residual_block(net, 6, stride=1, expansion=1, scope='block1_0')
+
+            dsn2 = net
+            net = _inverted_residual_block(net, 12, stride=2, scope='block2_0')  # size/4
+            net = _inverted_residual_block(net, 12, stride=1, scope='block2_1')
+
+            dsn3 = net
+            net = _inverted_residual_block(net, 24, stride=2, scope='block3_0')  # size/8
+            net = _inverted_residual_block(net, 24, stride=1, scope='block3_1')
+            net = _inverted_residual_block(net, 24, stride=1, scope='block3_2')
+
+            dsn4 = net
+            net = _inverted_residual_block(net, 48, stride=2, scope='block4_0')  # size/16
+            net = _inverted_residual_block(net, 48, stride=1, scope='block4_1')
+            net = _inverted_residual_block(net, 48, stride=1, scope='block4_2')
+            net = _inverted_residual_block(net, 48, stride=1, scope='block4_3')
+
+            net = _inverted_residual_block(net, 64, stride=1, scope='block5_0')
+            net = _inverted_residual_block(net, 64, stride=1, scope='block5_1')
+            net = _inverted_residual_block(net, 64, stride=1, scope='block5_2')
+
+            dsn5 = net
+
+        ## dsn layers
+        with tf.variable_scope('dsn1'):
+            dsn1 = _dsn_1x1_conv2d(dsn1, 1)
+            # print('!! debug, dsn1 shape is: {}'.format(dsn1.get_shape()))
+            ## no need deconv2d
+
+        with tf.variable_scope('dsn2'):
+            dsn2 = _dsn_1x1_conv2d(dsn2, 1)
+            # print('!! debug, dsn2 shape is: {}'.format(dsn2.get_shape()))
+            dsn2 = _dsn_deconv2d_with_upsample_factor(dsn2, 1, upsample_factor=2)
+            # print('!! debug, dsn2 shape is: {}'.format(dsn2.get_shape()))
+
+        with tf.variable_scope('dsn3'):
+            dsn3 = _dsn_1x1_conv2d(dsn3, 1)
+            # print('!! debug, dsn3 shape is: {}'.format(dsn3.get_shape()))
+            dsn3 = _dsn_deconv2d_with_upsample_factor(dsn3, 1, upsample_factor=4)
+            # print('!! debug, dsn3 shape is: {}'.format(dsn3.get_shape()))
+
+        with tf.variable_scope('dsn4'):
+            dsn4 = _dsn_1x1_conv2d(dsn4, 1)
+            # print('!! debug, dsn4 shape is: {}'.format(dsn4.get_shape()))
+            dsn4 = _dsn_deconv2d_with_upsample_factor(dsn4, 1, upsample_factor=8)
+            # print('!! debug, dsn4 shape is: {}'.format(dsn4.get_shape()))
+
+        with tf.variable_scope('dsn5'):
+            dsn5 = _dsn_1x1_conv2d(dsn5, 1)
+            # print('!! debug, dsn5 shape is: {}'.format(dsn5.get_shape()))
+            dsn5 = _dsn_deconv2d_with_upsample_factor(dsn5, 1, upsample_factor=16)
+            # print('!! debug, dsn5 shape is: {}'.format(dsn5.get_shape()))
+
+        # dsn fuse
+        with tf.variable_scope('dsn_fuse'):
+            dsn_fuse = tf.concat([dsn1, dsn2, dsn3, dsn4, dsn5], 3)
+            # print('debug, dsn_fuse shape is: {}'.format(dsn_fuse.get_shape()))
+            dsn_fuse = _output_1x1_conv2d(dsn_fuse, 1)
+            # print('debug, dsn_fuse shape is: {}'.format(dsn_fuse.get_shape()))
+
+    return dsn_fuse, dsn1, dsn2, dsn3, dsn4, dsn5
+
+
+
+
+
+
+
 
 
 
@@ -256,6 +388,217 @@ def mobilenet_v1_style_hed(inputs, batch_size, is_training):
             print('debug, dsn_fuse shape is: {}'.format(dsn_fuse.get_shape()))
 
     return dsn_fuse, dsn1, dsn2, dsn3, dsn4, dsn5
+
+
+
+def vgg_style_hed(inputs, batch_size, is_training):
+    filter_initializer = tf.contrib.layers.xavier_initializer()
+    if const.use_kernel_regularizer:
+        weights_regularizer = tf.contrib.layers.l2_regularizer(scale=0.0005)
+    else:
+        weights_regularizer = None
+
+    def _vgg_conv2d(inputs, filters, kernel_size):
+        use_bias = True
+        if const.use_batch_norm:
+            use_bias = False
+
+        outputs = tf.layers.conv2d(inputs, filters, kernel_size,
+                                   padding='same',activation=None,
+                                   use_bias=use_bias,
+                                   kernel_initializer=filter_initializer,
+                                   kernel_regularizer=weights_regularizer)
+        if const.use_batch_norm:
+            outputs = tf.layers.batch_normalization(outputs, training=is_training)
+        outputs = tf.nn.relu(outputs)
+        return outputs
+
+    def _max_pool2d(inputs):
+        outputs = tf.layers.max_pooling2d(inputs, [2, 2], strides=(2, 2), padding='same')
+        return outputs
+
+    def _dsn_1x1_conv2d(inputs, filters):
+        use_bias = True
+        if const.use_batch_norm:
+            use_bias = False
+        kernel_size = [1, 1]
+        outputs = tf.layers.conv2d(inputs, filters,kernel_size,
+                                   padding='same', activation=None,
+                                   use_bias=use_bias,
+                                   kernel_initializer=filter_initializer,
+                                   kernel_regularizer=weights_regularizer)
+        if const.use_batch_norm:
+            outputs = tf.layers.batch_normalization(outputs, training=is_training)
+
+        return outputs
+
+    def _output_1x1_conv2d(inputs, filters):
+        kernel_size = [1,1]
+        outputs = tf.layers.conv2d(inputs, filters, kernel_size, padding='same',
+                                   activation=None,
+                                   use_bias=True,
+                                   kernel_initializer=filter_initializer,
+                                   kernel_regularizer=weights_regularizer)
+        ## 不用batch normalization
+        ## 不同激活
+
+        return outputs
+
+    def _dsn_deconv2d_with_upsample_factor(inputs, filters, upsample_factor):
+        kernel_size = [2 * upsample_factor, 2 * upsample_factor]
+        outputs = tf.layers.conv2d_transpose(inputs, filters, kernel_size,
+                                             strides=(upsample_factor, upsample_factor),
+                                             padding='same',
+                                             activation=None,
+                                             use_bias=True,
+                                             kernel_initializer=filter_initializer,
+                                             kernel_regularizer=weights_regularizer)
+        # 概念上来说，deconv2d 已经是最后的输出 layer 了，
+        # 只不过最后还有一步 1x1 的 conv2d 把 5 个 deconv2d 的输出再融合到一起
+        # 所以不需要再使用 batch normalization 了
+        return outputs
+
+    with tf.variable_scope('hed', 'hed', [inputs]):
+        end_points = {}
+        net = inputs
+
+        with tf.variable_scope('conv1'):
+            net = _vgg_conv2d(net, 12, [3, 3])
+            net = _vgg_conv2d(net, 12, [3, 3])
+            dsn1 = net
+            net = _max_pool2d(net)
+
+        with tf.variable_scope('conv2'):
+            net = _vgg_conv2d(net, 24, [3, 3])
+            net = _vgg_conv2d(net, 24, [3, 3])
+            dsn2 = net
+            net = _max_pool2d(net)
+
+        with tf.variable_scope('conv3'):
+            net = _vgg_conv2d(net, 48, [3, 3])
+            net = _vgg_conv2d(net, 48, [3, 3])
+            net = _vgg_conv2d(net, 48, [3, 3])
+            dsn3 = net
+            net = _max_pool2d(net)
+
+        with tf.variable_scope('conv4'):
+            net = _vgg_conv2d(net, 96, [3, 3])
+            net = _vgg_conv2d(net, 96, [3, 3])
+            net = _vgg_conv2d(net, 96, [3, 3])
+            dsn4 = net
+            net = _max_pool2d(net)
+
+        with tf.variable_scope('conv5'):
+            net = _vgg_conv2d(net, 192, [3, 3])
+            net = _vgg_conv2d(net, 192, [3, 3])
+            net = _vgg_conv2d(net, 192, [3, 3])
+            dsn5 = net
+            # 此处不需要池化
+
+        #########【dsn layers边路层】#############
+        with tf.variable_scope('dsn1'):
+            dsn1 = _dsn_1x1_conv2d(dsn1, 1)
+            print('!! debug, dsn1 shape is: {}'.format(dsn1.get_shape()))
+            ## no need deconv2d
+
+        with tf.variable_scope('dsn2'):
+            dsn2 = _dsn_1x1_conv2d(dsn2, 1)
+            print('!! debug, dsn2 shape is: {}'.format(dsn2.get_shape()))
+            dsn2 = _dsn_deconv2d_with_upsample_factor(dsn2, 1, upsample_factor=2)
+            print('!! debug, dsn2 shape is: {}'.format(dsn2.get_shape()))
+        with tf.variable_scope('dsn3'):
+            dsn3 = _dsn_1x1_conv2d(dsn3, 1)
+            print('!! debug, dsn3 shape is: {}'.format(dsn3.get_shape()))
+            dsn3 = _dsn_deconv2d_with_upsample_factor(dsn3, 1, upsample_factor=4)
+            print('!! debug, dsn3 shape is: {}'.format(dsn3.get_shape()))
+
+        with tf.variable_scope('dsn4'):
+            dsn4 = _dsn_1x1_conv2d(dsn4, 1)
+            print('!! debug, dsn4 shape is: {}'.format(dsn4.get_shape()))
+            dsn4 = _dsn_deconv2d_with_upsample_factor(dsn4, 1, upsample_factor=8)
+            print('!! debug, dsn4 shape is: {}'.format(dsn4.get_shape()))
+
+        with tf.variable_scope('dsn5'):
+            dsn5 = _dsn_1x1_conv2d(dsn5, 1)
+            print('!! debug, dsn5 shape is: {}'.format(dsn5.get_shape()))
+            dsn5 = _dsn_deconv2d_with_upsample_factor(dsn5, 1, upsample_factor=16)
+            print('!! debug, dsn5 shape is: {}'.format(dsn5.get_shape()))
+        #################【将边路层融合】###################
+        with tf.variable_scope('dsn_fuse'):
+            dsn_fuse = tf.concat([dsn1, dsn2, dsn3, dsn4, dsn5], 3)
+            print('debug, dsn_fuse shape is: {}'.format(dsn_fuse.get_shape()))
+            dsn_fuse = _output_1x1_conv2d(dsn_fuse, 1)
+            print('debug, dsn_fuse shape is: {}'.format(dsn_fuse.get_shape()))
+
+    return dsn_fuse, dsn1, dsn2, dsn3, dsn4, dsn5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
